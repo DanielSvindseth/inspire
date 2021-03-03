3f8eaf2e7c4204b7e19c19d5526b9276fb458634
Daniel <daniel@svindseth.net>
2021-03-03 22:47:36 +0100

    removed mangekant

--- scripts/figurleik.js
+++ scripts/figurleik.js
@@ -2,7 +2,7 @@ function id(id) {
   return document.getElementById(id); }
 
 // Talet på slides +1
-var numberOfSlides = '13';
+var numberOfSlides = '12';
 
 Mousetrap.bind('o', function() { location.href = '#/overview' } );
 Mousetrap.bind('i', function() { location.href = '#/TOC'; } );
