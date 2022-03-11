5db7bec6230307faa715b16ebed371bae015ca54
Daniel Svindseth <daniel@svindseth.net>
2022-03-11 07:24:37 +0100

    Added memory

--- scripts/random_number.js
+++ scripts/random_number.js
@@ -1,9 +1,60 @@
 function id(id) {
   return document.getElementById(id); }
 
-function getRandomNumber() {
-  a = Math.floor(Math.random() * 50) + 1;
+let min = 1;
+let max = 50;
+const possibleNumbers = [];
+const usedNumbers = [];
+const list = [];
+
+function randomizer() {
+  let a = Math.floor(Math.random() * max) + min;
   return a;
+}
+
+function getRandomNumber() {
+  a = randomizer();
+  rememberNumber(a);
+  return a;
+}
+
+function getRandomNumberNew() {
+  if (id("repeat-checkbox").checked == true) {
+    a = randomizer();
+    for (var i = 0; i < list.length; i++) {
+      if (list[i] == a) {
+        console.log('duplicate found, trying again');
+        getRandomNumber();
+      }
+      else {
+        list[list.length] = a;
+        console.log(list);
+        rememberNumber(a);
+        return a;
+      }
+    }
+
+  }
+  else if (id("repeat-checkbox").checked == false) {
+    a = randomizer();
+    list[list.length] = a;
+    console.log(list);
+    rememberNumber(a);
+    return a;
+  }
+}
+
+function prepareList() {
+  for (var i = 0; i < max; i++) {
+    possibleNumbers[possibleNumbers.length] = i + 1;
+  }
+  console.log(possibleNumbers);
+}
+
+function rememberNumber(e) {
+  let f = e + "<br>"
+  //id("memory-child").innerHTML += f;
+  id("memory-child").insertAdjacentHTML("afterbegin", f);
 }
 
 Mousetrap.bind('space', function() {
