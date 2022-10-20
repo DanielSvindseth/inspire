6553343902ff13f83e6ec01079a7cea953d4e08c
Daniel Svindseth <daniel@svindseth.net>
2022-10-20 11:42:34 +0200

    changed localStorage keys to strings, hopefully this works

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -1,46 +1,44 @@
 function id(id) {
   return document.getElementById(id); }
 
-var decimal = 0;
-
-localStorage.setItem(numbers1_number, 10);
-localStorage.setItem(numbers1_decimal, 1);
+localStorage.setItem('numbers1_number', 10);
+localStorage.setItem('numbers1_decimal', 1);
 
 function increase() {
-    var n = parseInt(localStorage.getItem(numbers1_number));
+    var n = parseInt(localStorage.getItem('numbers1_number'));
     n = parseInt(n + 1);
     var d = n/10;
-    localStorage.setItem(numbers1_number, n);
-    localStorage.setItem(numbers1_decimal, d);
+    localStorage.setItem('numbers1_number', n);
+    localStorage.setItem('numbers1_decimal', d);
     console.log(d);
 }
 
 function decrease() {
-    var n = parseInt(localStorage.getItem(numbers1_number));
+    var n = parseInt(localStorage.getItem('numbers1_number'));
     n = parseInt(n - 1);
     var d = n/10;
-    localStorage.setItem(numbers1_number, n);
-    localStorage.setItem(numbers1_decimal, d);
+    localStorage.setItem('numbers1_number', n);
+    localStorage.setItem('numbers1_decimal', d);
     console.log(d);
 }
 
 
 Mousetrap.bind('right', function() {
   increase();
-  id("number").innerHTML = localStorage.getItem(numbers1_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers1_decimal');
 } );
 
 Mousetrap.bind('pagedown', function() {
   increase();
-  id("number").innerHTML = localStorage.getItem(numbers1_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers1_decimal');
 } );
 
 Mousetrap.bind('pageup', function() {
   decrease();
-  id("number").innerHTML = localStorage.getItem(numbers1_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers1_decimal');
 } );
 
 Mousetrap.bind('left', function() {
   decrease();
-  id("number").innerHTML = localStorage.getItem(numbers1_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers1_decimal');
 } );
--- scripts/numbers2.js
+++ scripts/numbers2.js
@@ -1,47 +1,44 @@
 function id(id) {
   return document.getElementById(id); }
 
-var decimal = 0;
-
-
-localStorage.setItem(numbers2_number, 100);
-localStorage.setItem(numbers2_decimal, 1);
+localStorage.setItem('numbers2_number', 100);
+localStorage.setItem('numbers2_decimal', 1);
 
 function increase() {
-    var n = parseInt(localStorage.getItem(numbers2_number));
+    var n = parseInt(localStorage.getItem('numbers2_number'));
     n = parseInt(n + 1);
     var d = n/100;
-    localStorage.setItem(numbers2_number, n);
-    localStorage.setItem(numbers2_decimal, d);
+    localStorage.setItem('numbers2_number', n);
+    localStorage.setItem('numbers2_decimal', d);
     console.log(d);
 }
 
 function decrease() {
-    var n = parseInt(localStorage.getItem(numbers2_number));
+    var n = parseInt(localStorage.getItem('numbers2_number'));
     n = parseInt(n - 1);
     var d = n/100;
-    localStorage.setItem(numbers2_number, n);
-    localStorage.setItem(numbers2_decimal, d);
+    localStorage.setItem('numbers2_number', n);
+    localStorage.setItem('numbers2_decimal', d);
     console.log(d);
 }
 
 
 Mousetrap.bind('right', function() {
   increase();
-  id("number").innerHTML = localStorage.getItem(numbers2_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers2_decimal');
 } );
 
 Mousetrap.bind('pagedown', function() {
   increase();
-  id("number").innerHTML = localStorage.getItem(numbers2_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers2_decimal');
 } );
 
 Mousetrap.bind('pageup', function() {
   decrease();
-  id("number").innerHTML = localStorage.getItem(numbers2_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers2_decimal');
 } );
 
 Mousetrap.bind('left', function() {
   decrease();
-  id("number").innerHTML = localStorage.getItem(numbers2_decimal);
+  id("number").innerHTML = localStorage.getItem('numbers2_decimal');
 } );
